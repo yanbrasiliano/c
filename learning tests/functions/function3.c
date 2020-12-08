62038576e9dffced556a84a75de4e2c9562f5a0d
yanbrasiliano <yanpenabr@gmail.com>
2020-12-08 18:08:06 -0300

    Update function3.c

--- learning tests/functions/function3.c
+++ learning tests/functions/function3.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
+int varglobal = 2;
+
 void test(void)
 {
 
     int variavellocalautomatica = 5;
-     variavellocalautomatica*= 3;
+    variavellocalautomatica *= 3;
 
     static int variavellocalstatic = 5;
-    variavellocalstatic *= 3 ;
+    variavellocalstatic *= 3;
 
-    printf("Local Automático >> %i\n",variavellocalautomatica);
-    printf("Local Estático >> %i\n",variavellocalstatic);
+    varglobal *= 5;
 
+    printf("Local Automático >> %i\n", variavellocalautomatica);
+    printf("Local Estático >> %i\n", variavellocalstatic);
+    printf("Global >> %i\n", varglobal);
 }
 
 int main(void)
 {
     // programa para testar static nas funções.
-   
-    test();
-    test();
-    test();
 
+    printf("Global >> %i\n", varglobal);
+    test();
+    test();
 
     return 0;
 }
