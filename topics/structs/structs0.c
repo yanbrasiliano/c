46efc93d8af441961247980c8e2ddd54884ddc2f
yanbrasiliano <yanpenabr@gmail.com>
2020-12-14 21:47:09 -0300

    Update structs0.c

--- topics/structs/structs0.c
+++ topics/structs/structs0.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 
 //struct básico para entendimento do tópico.
-int main()
+
+struct hour
 {
-	struct hour
-	{
-		int hours;
-		int minutes;
-		int seconds;
-	};
+	int hours;
+	int minutes;
+	int seconds;
+};
+
+
+int main(void)
+{
+
+	struct hour test(struct hour x);
 
 	struct hour now;
-
 	now.hours = 12;
 	now.minutes = 10;
 	now.seconds = 3;
+	
+	struct hour after;
+	after = test(now);
 
-	printf("%i:%i:%i\n\n", now.hours, now.minutes, now.seconds);
+	printf("%i:%i:%i\n", now.hours, now.minutes, now.seconds);
+	printf("%i:%i:%i\n", after.hours, after.minutes, after.seconds);
 
 	printf("Press ENTER for exit!");
 
-			getchar();
+	getchar();
 
 	return 0;
+
+}
+
+struct hour test(struct hour x)
+{
+
+//printf("%i:%i:%i\n", x.hours,x.minutes,x.seconds);
+
+x.hours = 10;
+x.minutes = 5;
+x.seconds = 3;
+
+	return x;
 }
