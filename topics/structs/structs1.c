12766d9c8b81491bae5ce477b713d9861db4266a
yanbrasiliano <yanpenabr@gmail.com>
2020-12-15 23:54:09 -0300

    Create structs1.c

---
+++ topics/structs/structs1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+
+struct hour // estrutura global
+{
+	int hours;
+	int minutes;
+	int seconds;
+};
+
+void recHours(struct hour [5]){
+
+	int, a,b,c;
+
+	for(x = ; x < 5; x++){
+		printf("\nHours -> ");
+		scanf("%d", &);
+		printf("\nMinutes -> ");
+		scanf();
+		printf("\nSeconds -> ");
+		scanf();
+}
+
+
+}
+
+void printHour(struct hour [5]){
+
+
+}
+
+int main(void){
+
+struct hour list[5];
+int x;
+
+recHours(list);
+printHour(list);
+
+
+return 0;
+}
