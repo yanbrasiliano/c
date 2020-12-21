7c0e97a41a263fff2537b61df0e11288b7100a56
yanbrasiliano <yanpenabr@gmail.com>
2020-12-21 18:12:41 -0300

    Update sizeof.c

--- topics/varied/sizeof.c
+++ topics/varied/sizeof.c
@@ -3,15 +3,18 @@
 int main(void)
 {
 
-	int number = 10;
+	int number = 10; // função sizeof retorna o valor em bytes da variável
 	float note = 5.5;
 	char words = 'd';
 	double other = 12.3;
+	
 
-
-	printf("The variable 'number' have a value %d and takes up the space %lf", number,sizeof(number));
-	printf("The variable 'number' have a value %.2f and takes up the space %lf", note,sizeof(note));
-	printf("The variable 'number' have a value %c and takes up the space %lf", words,sizeof(words));
-	printf("The variable 'number' have a value %.2f and takes up the space %lf", other,sizeof(other));
-			return 0;
+		printf("The variable 'number' have a value %d and takes up the space %ld\n", number, sizeof(number));
+		printf("The variable 'note' have a value %.2f and takes up the space %ld\n", note, sizeof(note));
+		printf("The variable 'words' have a value %c and takes up the space %ld\n", words, sizeof(words));
+		printf("The variable 'other' have a value %.2f and takes up the space %ld\n", other, sizeof(other));
+	
+	
+	
+	return 0;
 }
