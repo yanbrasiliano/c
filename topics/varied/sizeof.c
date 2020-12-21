2548e640425e666740f53ee0182cb4e857158923
yanbrasiliano <yanpenabr@gmail.com>
2020-12-21 18:06:44 -0300

    Create sizeof.c

---
+++ topics/varied/sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int number = 10;
+	float note = 5.5;
+	char words = 'd';
+	double other = 12.3;
+
+
+	printf("The variable 'number' have a value %d and takes up the space %lf", number,sizeof  );
+	printf("");
+	printf("");
+	printf("");
+
+			return 0;
+}
