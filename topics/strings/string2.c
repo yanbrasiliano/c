2728001adac92e7b3086205b13ca9f1fd6bd6b9e
yanbrasiliano <yanpenabr@gmail.com>
2020-12-22 17:14:11 -0300

    Update string2.c

--- topics/strings/string2.c
+++ topics/strings/string2.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
+
+int tString(char string[]){
+
+	int numString = 0;
+
+
+	while (string[numString] != '\0'){
+		numString++;
+
+	}
+	return numString;
+}
+
 int main(void){
 
-void concatString(char s1[], char s2[], char s3[], int t1, int t2);
-
-char p1 [] = {'H', 'e', 'l','l','o', ' '};
-char p2 [] = {'W','o','r','l','d'};
-char nWord [11];
+int tString(char string[]);
+char name[20];
 
 
-concatString (p1,6,p2,5,nWord);
-
-for (int i = 0; i < 11; i++){
-
-printf("%c", nWord[i]);
-
-}
 
 
-printf("\n");
-
-	return 0;
-
-}
+printf("type word(string): ");
+scanf("%s", &name);
 
 
-void concatString(char s1[], char s2[], char s3[], int t1, int t2){
+int num = tString(name);
 
-int i,j;
+printf(" The %s have %i chars. \n",name, num);
 
-	for (i = 0; i < t1 ; ++i){
-	s3[i] = s1[i];
-	}
-	for (j = 0; j < t2 ; ++j){
-	s3[t1 + j] = s2[j];
-	}
+return 0;
 
 }
